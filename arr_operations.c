48998bbc4ccde0a033545480e9df62ee26db65d6
Neeraj <neeraj79108@gmail.com>
2022-02-20 16:00:53 +0530

    added linear search  method

--- arr_operations.c
+++ arr_operations.c
@@ -30,17 +30,35 @@ int IndesxInsertion(int arr[], int size, int capacity, int index, int elememnt)
 // Deletion
 void Deletion(int arr[], int size, int del_index)
 {
-    for (int i = del_index; i < size-1; i++)
+    for (int i = del_index; i < size - 1; i++)
     {
         arr[del_index] = arr[del_index + 1];
     }
     printf("\nDeletion has been done without any error -- :)\n");
 }
 
+// Linaer Serach
+void Linear_Search(int arr[], int size, int element)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == element)
+        {
+            printf("\nGiven element is in index number : %d", i);
+            break;
+        }
+        else if (i == size)
+        {
+            printf("Element not found");
+            break;
+        }
+    }
+}
 int main()
 {
     int arr[100] = {7, 25, 3, 1, 33};
     int size = 5;
+    Linear_Search(arr, size, 3);
     IndesxInsertion(arr, size, 100, 3, 5);
     size += 1;
     display(arr, size);
